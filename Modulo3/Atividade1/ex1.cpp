4bd5ead95f38596390a86b1418059c859f291858
Bruno Paes <bpaes.developer@gmail.com>
2021-04-16 12:54:25 -0300

    adicionado atividade 1 do modulo 3

---
+++ Modulo3/Atividade1/ex1.cpp
@@ -0,0 +1,36 @@
+/* Bruno Paes
+ * Prog II
+ *
+ * Modulo 3
+ * Atividade 1
+ *
+ * Exercicio 1
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using std::string;
+using namespace std;
+
+class Produto {
+private:
+	float preco;
+	string nome;
+};
+
+class ComboProduto {
+private:
+	float totalValor;
+	int qtdItens;
+};
+
+int main() {
+
+	
+	return 0;
+}
+
---
+++ Modulo3/Atividade1/ex2.cpp
@@ -0,0 +1,28 @@
+/* Bruno Paes
+ * Prog II
+ *
+ * Modulo 3
+ * Atividade 1
+ *
+ * Exercicio 2
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using std::string;
+using namespace std;
+
+class FormaGeometrica{ //ABSTRATA
+	virtual float calculaArea() {}
+}
+
+int main() {
+
+
+	return 0;
+}
+
